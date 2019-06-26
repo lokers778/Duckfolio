bd0ee5f71b78c5c15120953144724dee0dd5a3cf
lokers778 <lokers778@gmail.com>
2019-06-26 18:57:57 +0200

    added header

--- js/components/Header.js
+++ js/components/Header.js
@@ -1,7 +1,62 @@
 import React from "react"
 
+function Duck() {
+    return (
+        <>
+        <div className="duck">
+            <div className="duckBody">
+                <div className="leftWing"></div>
+                <div className="rightWing"></div>
+                <div className="duckHead">
+                    <div className="rightEye"></div>
+                    <div className="leftEye"></div>
+                    <div className="upperBeak"></div>
+                    <div className="bottomBeak"></div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+       </>
+    )
+}
+
+function Logo() {
+    return (
+        <>
+            <div className="logo">
+                <h1>DuckFolio</h1>
+            </div>
+        </>
+    )
+}
+function Nav() {
+    return (
+        <>
+           <nav>
+           </nav>
+        </>
+    )
+}
+
+
+
+
+
 function Header() {
-    return (<h1>this is header</h1>)
+    return (
+        <header>
+        <div className="container">
+            <Duck />
+            <Logo/>
+           <Nav/>
+        </div>
+        </header>
+            )
 }
 
 export default Header
