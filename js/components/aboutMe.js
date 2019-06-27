ec66ca189a2094310f9e29e8b3677a0465c52a16
lokers778 <lokers778@gmail.com>
2019-06-27 11:54:57 +0200

    added about me section

--- js/components/aboutMe.js
+++ js/components/aboutMe.js
@@ -1,7 +1,22 @@
 import React from "react"
 
 function AboutMe() {
-    return (<h1>this is aboutme</h1>)
+    return (
+        <section className="aboutMeSection">
+        <div className="container">
+            <div className="myDescription">
+            <div className="buttonTabs">
+                <button>About Me</button>
+                <button>My skils</button>
+                <button>Why duck</button>
+            </div>
+            <div id="AboutMe" className="description"></div>
+            <div id="My skils" className="description"></div>
+            <div id="My story" className="description"></div>
+            </div>
+        </div>
+        </section>
+    )
 }
 
 export default AboutMe
--- js/components/duckApp.js
+++ js/components/duckApp.js
@@ -4,14 +4,14 @@ function DuckApp() {
     return (
         <section className="app">
         <div className="container">
-
+            <h2>DuckApp</h2>
             <div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-                <div>App</div>
-    </div>
+                <div><span className="icon"><i className="fas fa-calculator"></i></span><span>Calculator</span></div>
+                <div><span className="icon"><i className="fas fa-list"></i></span><span>Todo list</span></div>
+                <div><span className="icon"><i className="fas fa-image"></i></span><span>project</span></div>
+            <div><span className="icon"><i className="fas fa-image"></i></span><span>project</span></div>
+            <div><span className="icon"><i className="fab fa-react"></i></span><span>React App</span></div>
+        </div>
         </div>
         </section>
     )
