a03e7ebc06d06408d143fb40d6da1bf8b135ae09
lokers778 <lokers778@gmail.com>
2019-06-27 12:35:02 +0200

    added buttons to about me , better component order

--- js/components/aboutMe.js
+++ js/components/aboutMe.js
@@ -1,20 +1,64 @@
 import React from "react"
 
+function AboutMeComponent() {
+    return (
+        <div id="AboutMe" className="description">
+            <div className="horizontalTabsButton">
+                <button>My persona</button>
+                <button>Education</button>
+                <button>Experience</button>
+                <button>Plans</button>
+                <button>Hobby</button>
+            </div>
+            <div className="AboutMeDescriptions"></div>
+        </div>
+    )
+}
+
+function MySkillsComponent() {
+    return (
+        <div id="MySkills" className="description">
+            <div className="horizontalTabsButton">
+                <button>HTML</button>
+                <button>CSS</button>
+                <button>JS</button>
+                <button>JQuery</button>
+                <button>React</button>
+                <button>Tech</button>
+                <button>General</button>
+            </div>
+            <div className="MySkillDescriptions"></div>
+        </div>
+    )
+}
+
+function WhyDuckComponent() {
+    return (
+        <div id="My story" className="description"></div>
+    )
+}
+
+function ButtonTabs() {
+    return(
+        <div className="buttonTabs">
+            <button>About Me</button>
+            <button>My Skills</button>
+            <button>Why Duck</button>
+        </div>
+    )
+}
+
 function AboutMe() {
     return (
         <section className="aboutMeSection">
-        <div className="container">
-            <div className="myDescription">
-            <div className="buttonTabs">
-                <button>About Me</button>
-                <button>My skils</button>
-                <button>Why duck</button>
+            <div className="container">
+                <div className="myDescription">
+                   <ButtonTabs/>
+                    <AboutMeComponent/>
+                    <MySkillsComponent/>
+                    <WhyDuckComponent/>
+                </div>
             </div>
-            <div id="AboutMe" className="description"></div>
-            <div id="My skils" className="description"></div>
-            <div id="My story" className="description"></div>
-            </div>
-        </div>
         </section>
     )
 }
