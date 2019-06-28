d1099b8749de835f08ef31cf0c149c517d51f53f
lokers778 <lokers778@gmail.com>
2019-06-28 12:23:29 +0200

    added horizontal tabs

--- js/components/aboutMe.js
+++ js/components/aboutMe.js
@@ -1,31 +1,82 @@
 import React, {Component} from "react"
 
 class AboutMeComponent extends Component {
-
+    state = {
+        display1: "none",
+        display2: "none",
+        display3: "none",
+        display4: "none",
+        display5: "none",
+    }
+    handeClick1 = () => {
+        this.setState({
+            display1: "block",
+            display2: "none",
+            display3: "none",
+            display4: "none",
+            display5: "none",
+        })
+    }
+    handeClick2=()=> {
+        this.setState({
+            display1: "none",
+            display2: "block",
+            display3: "none",
+            display4: "none",
+            display5: "none",
+        })
+    }
+    handeClick3=()=> {
+        this.setState({
+            display1: "none",
+            display2: "none",
+            display3: "block",
+            display4: "none",
+            display5: "none",
+        })
+    }
+    handeClick4=()=> {
+        this.setState({
+            display1: "none",
+            display2: "none",
+            display3: "none",
+            display4: "block",
+            display5: "none",
+        })
+    }
+    handeClick5=()=> {
+        this.setState({
+            display1: "none",
+            display2: "none",
+            display3: "none",
+            display4: "none",
+            display5: "block",
+        })
+    }
     render() {
         return (
-            <div id="AboutMe" className="description" style={{display:this.props.block}}>
-                <div className="horizontalTabsButton" >
-                    <button>My persona</button>
-                    <button>Education</button>
-                    <button>Experience</button>
-                    <button>Plans</button>
-                    <button>Hobby</button>
+            <div id="AboutMe" className="description" style={{display: this.props.block}}>
+                <div className="horizontalTabsButton">
+                    <button onClick={this.handeClick1}>My persona</button>
+                    <button onClick={this.handeClick2}>Education</button>
+                    <button onClick={this.handeClick3}>Experience</button>
+                    <button onClick={this.handeClick4}>Plans</button>
+                    <button onClick={this.handeClick5}>Hobby</button>
                 </div>
                 <div className="AboutMeDescriptions">
-                    <div className="MyPersona">
+                    <div className="MyPersona" style={{display:this.state.display1}}>
                         1
                     </div>
-                    <div className="Education">
+                    <div className="Education" style={{display:this.state.display2}}>
                         2
                     </div>
-                    <div className="Experience">
+                    <div className="Experience" style={{display:this.state.display3}}>
                         3
                     </div>
-                    <div className="Plans">
+                    <div className="Plans" style={{display:this.state.display4}}>
                         4
                     </div>
-                    <div className="Hobby">
+                    <div className="Hobby" style={{display:this.state.display5}}>
                         5
                     </div>
 
@@ -36,32 +87,83 @@ class AboutMeComponent extends Component {
 }
 
 class MySkillsComponent extends Component {
-
+    state = {
+        display1: "none",
+        display2: "none",
+        display3: "none",
+        display4: "none",
+        display5: "none",
+    }
+    handeClick1 = () => {
+        this.setState({
+            display1: "block",
+            display2: "none",
+            display3: "none",
+            display4: "none",
+            display5: "none",
+        })
+    }
+    handeClick2=()=> {
+        this.setState({
+            display1: "none",
+            display2: "block",
+            display3: "none",
+            display4: "none",
+            display5: "none",
+        })
+    }
+    handeClick3=()=> {
+        this.setState({
+            display1: "none",
+            display2: "none",
+            display3: "block",
+            display4: "none",
+            display5: "none",
+        })
+    }
+    handeClick4=()=> {
+        this.setState({
+            display1: "none",
+            display2: "none",
+            display3: "none",
+            display4: "block",
+            display5: "none",
+        })
+    }
+    handeClick5=()=> {
+        this.setState({
+            display1: "none",
+            display2: "none",
+            display3: "none",
+            display4: "none",
+            display5: "block",
+        })
+    }
     render() {
         return (
-            <div id="MySkills" className="description" style={{display:this.props.block}}>
-                <div className="horizontalTabsButton" >
-                    <button>HTML/CSS</button>
-                    <button>JS/JQuery</button>
-                    <button>React</button>
-                    <button>Tech</button>
-                    <button>General</button>
+            <div id="MySkills" className="description" style={{display: this.props.block}}>
+                <div className="horizontalTabsButton">
+                    <button onClick={this.handeClick1}>HTML/CSS</button>
+                    <button onClick={this.handeClick2}>JS/JQuery</button>
+                    <button onClick={this.handeClick3}>React</button>
+                    <button onClick={this.handeClick4}>Tech</button>
+                    <button onClick={this.handeClick5}>General</button>
                 </div>
                 <div className="MySkillDescriptions">
-                    <div className="htmlCSS">
+                    <div className="htmlCSS" style={{display:this.state.display1}}>
                         11
                     </div>
 
-                    <div className="jsJquery">
+                    <div className="jsJquery" style={{display:this.state.display2}}>
                         22
                     </div>
-                    <div className="React">
+                    <div className="React" style={{display:this.state.display3}}>
                         33
                     </div>
-                    <div className="Tech">
+                    <div className="Tech" style={{display:this.state.display4}}>
                         44
                     </div>
-                    <div className="General">
+                    <div className="General" style={{display:this.state.display5}}>
                         55
                     </div>
                 </div>
@@ -75,7 +177,7 @@ class WhyDuckComponent extends Component {
 
     render() {
         return (
-            <div id="My story" className="description" style={{display:this.props.block}}>
+            <div id="My story" className="description" style={{display: this.props.block}}>
                 <h2>Why Ducks</h2>
                 <p>Because i love them</p>
             </div>
@@ -84,18 +186,19 @@ class WhyDuckComponent extends Component {
 }
 
 class ButtonTabs extends Component {
-    wasClicked1=()=>{
+    wasClicked1 = () => {
         this.props.changeDescription1();
 
     };
-    wasClicked2=()=>{
+    wasClicked2 = () => {
         this.props.changeDescription2();
 
-    } ;
-    wasClicked3=()=>{
+    };
+    wasClicked3 = () => {
         this.props.changeDescription3();
 
     }
+
     render() {
         return (
             <div className="buttonTabs">
@@ -104,42 +207,46 @@ class ButtonTabs extends Component {
                 <button onClick={this.wasClicked3}>Why Duck</button>
             </div>
         )
+
     }
 }
 
 class AboutMe extends Component {
-    state={
-        display1:"none",
-        display2:"none",
-        display3:"none",
+    state = {
+        display1: "none",
+        display2: "none",
+        display3: "none",
     }
-    changeDescription1=()=>{
-      this.setState({
-          display1 :"block",
-          display2:"none",
-          display3:"none",
-      })
-    }
-    changeDescription2=()=>{
+    changeDescription1 = () => {
         this.setState({
-            display1 :"none",
-            display2:"block",
-            display3:"none",
+            display1: "block",
+            display2: "none",
+            display3: "none",
         })
     }
-    changeDescription3=()=>{
+    changeDescription2 = () => {
         this.setState({
-            display1 :"none",
-            display2:"none",
-            display3:"block",
+            display1: "none",
+            display2: "block",
+            display3: "none",
         })
     }
+    changeDescription3 = () => {
+        this.setState({
+            display1: "none",
+            display2: "none",
+            display3: "block",
+        })
+    }
+
     render() {
         return (
             <section className="aboutMeSection">
                 <div className="container">
                     <div className="myDescription">
-                        <ButtonTabs changeDescription1={this.changeDescription1} changeDescription2={this.changeDescription2} changeDescription3={this.changeDescription3}/>
+                        <ButtonTabs changeDescription1={this.changeDescription1}
+                                    changeDescription2={this.changeDescription2}
+                                    changeDescription3={this.changeDescription3}/>
 
                         <AboutMeComponent block={this.state.display1}/>
                         <MySkillsComponent block={this.state.display2}/>
