a1b7b928b0590f1566a263f8e1dc82a472770450
lokers778 <lokers778@gmail.com>
2019-07-01 12:05:59 +0200

    added add task to task list funciotn

--- js/components/ToDoList.js
+++ js/components/ToDoList.js
@@ -1,50 +1,71 @@
 import React, {Component} from "react";
 import toDoData from "../data/ToDoList_data";
 
-class ToDoItem extends Component {
-    render() {
-        return (
-            <div className="todo-item">
-                <input type="checkbox" checked={this.props.task.completed} onChange={() => {
-                    this.props.handleChange(this.props.task.id)
-                }}/>
-                <p>{this.props.task.text}</p>
-                <button>remove</button>
-            </div>
-        )
-    }
-}
 
 
-class ToDoListComponent extends Component {
+
+class ToDoFormComponent extends Component{
     state = {
-        input :"",
-        doneTas: toDoData
+        text :"",
+        id:1,
     }
 
     handleChange=(event)=>{
         this.setState({
-        [event.target.name]:event.target.value
+            [event.target.name]:event.target.value
+        })
+    };
+
+    handleSubmit = (event)=>{
+        event.preventDefault();
+        this.props.onSubmit({
+            text:this.state.text,
+            complete:false,
+            id:this.state.id
+        })
+        this.setState({
+            id:this.state.id +1,
+            text: "",
         })
     }
-        handleSubmit = (event)=>{
-        event.preventDefault()
-        }
-
-
-
 
     render() {
-        const toDoArray = this.state.doneTas.map((task) => <ToDoItem key={task.id} task={task}
-                                                                     handleChange={this.handleChange}/>)
+        return<>
+            <h2>To Do List</h2>
+            <form onSubmit={this.handleSubmit}>
+                <input name="text" placeholder="add task" value={this.state.text} onChange={this.handleChange}/>
+                <button type="submit" onClick={this.handleSubmit}></button>
+            </form>
+        </>
+    }
+}
+
+
+class NewToDo extends Component{
+    render() {
+        return(
+            <div>{this.props.text}</div>
+        )
+
+    }
+}
+
+class ToDoListComponent extends Component {
+    state = {
+        toDoList:[],
+    };
+
+    newToDo=(task)=>{
+        this.setState({
+            toDoList: [task, ...this.state.toDoList]
+        })
+    };
+
+    render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                <h2>To Do List</h2>
-                {toDoArray}
-                <input name="text" placeholder="add task" value={this.state.text} onChange={this.handleChange}/>
-                <submit></submit>
-                </form>
+              <ToDoFormComponent onSubmit={this.newToDo}/>
+              {this.state.toDoList.map((task)=>(<NewToDo key={task.id} text={task.text}/>))}
             </div>
         )
 
